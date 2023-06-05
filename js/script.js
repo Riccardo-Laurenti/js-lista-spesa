eab07a8ce1b4a81811351ae7fb418fb2a5c46d0a
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-06-05 15:39:54 +0200

    finish exercise for cicle for

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 /*   Consegna:
      Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
-     Consigli:
+       Consigli:
 
-     -Facciamo un primo giro col for, in modo da sentirci confident sul risultato della stampa, poi trasformiamo il for in un while
+     - Facciamo un primo giro col for, in modo da sentirci confident sul risultato della stampa, poi trasformiamo il for in un while
      - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
      - Ricordiamoci di incrementare il contatore all'interno ciclo while per evitare di cascare in un loop infinito.
 */
+
+
+ // Recupero elemento dal Dom tramide ID
+ const recList = document.getElementById('shop-list');
+
+ const listShop = ['Latte', 'Yogurt', 'Marmellata', 'Frumento'];
+
+
+ let shopV = '';
+
+ // Creo ciclo for
+ for ( i = 0; i < listShop.length; i++ ) {
+    shopV += `<li> ${listShop[i]} </li> ` ;
+ }
+
+ 
+ // Stampo in pagina
+ recList.innerHTML = shopV;
